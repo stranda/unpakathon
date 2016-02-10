a6541fc71248a72792cf4ee4de6843df5b71509d
Allan Strand <stranda@gmail.com>
2016-02-10 12:47:11 -0500

    stuff

--- inst/dataclean/screen-data.R
+++ inst/dataclean/screen-data.R
@@ -2,7 +2,8 @@
 ##
 library(unpakathon)
 phenos <- unique(phenolong$variable)
-lapply(phenos,function(x)
-       {
-           
-       })
+outliertbl <- do.call(rbind,lapply(phenos,function(x)
+                                   {
+                                       findOutlier(phenolong,x,0.01)
+                                   })
+                      )

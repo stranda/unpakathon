74d3233f937534a4896b07114f9728ee16c6d0b2
allan strand <stranda@gmail.com>
2016-02-22 15:36:18 -0500

    data cleaning works now

--- inst/dataclean/screen-data.R
+++ inst/dataclean/screen-data.R
@@ -9,4 +9,11 @@ outlierlst <- lapply(phenos,function(x)
                          findOutlier(phenolong,x,0.01)
                      })
               
-sapply(outlierlst,dim)
+#sapply(outlierlst,dim)
+
+tmp <- lapply(outlierlst,function(x){
+    if (dim(x)[1]>0)
+    {
+        write.csv(file=paste0(x$variable[1],"-outliers.csv"),row.names=F,x)
+    }
+})

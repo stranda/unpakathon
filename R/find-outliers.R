c82c58554b3455c16fbe2a7de851dc69ef074a0b
allan strand <stranda@gmail.com>
2017-04-03 13:39:07 -0400

    Outliers

--- R/find-outliers.R
+++ R/find-outliers.R
@@ -3,15 +3,18 @@
 #' @param pl phenotype file (long)
 #' @param pheno is a phenotype
 #' @param q the quantile
+#' @param z.score if true converts the phenotypes to z-scores scaled by the sd in their own growth-chamber
 #' @import dplyr
 #' 
 #' @export
-findOutlier <- function(pl,pheno,q=0.01)
+findOutlier <- function(pl,pheno,q=0.01,z.score=F)
 {
     tmpdf <- pl%>%filter(variable%in%pheno)
+    if (z.score) tmpdf <- scalePhenos(classifier=c("treatment","facility","experiment"),dat=tmpdf,lineid="accession")
+    tmpdf$value[!is.finite(tmpdf$value)] <- NA
     tmpdf$direction <- NA
-    lbound <- quantile(pl$value,q)
-    hbound <- quantile(pl$value,1-q)
+    lbound <- quantile(tmpdf$value,q,na.rm=T)
+    hbound <- quantile(tmpdf$value,1-q,na.rm=T)
     small <- tmpdf%>%filter(value<lbound)
     if (dim(small)[1]>0) small$direction <- "low"
     large <- tmpdf%>%filter(value>hbound)
--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # unpakathon
 Data snapshot of unpak data with supporting functions and vignettes.
 
-This package depends on  'adjustPhenotypes'. You can install from github using:
+Make sure to install these packages: devtools, dplyr, ggplot2, igraph
 
-install_github("stranda/adjustPhenotypes")
+This package depends on  'adjustPhenotypes'. You can install from github using the incomparable devtools package:
+
+devtools::install_github("stranda/adjustPhenotypes")
+
+After installing adjustPhenotypes, you can install this package:
+
+devtools::install_github("stranda/unpakathon",force=T,build_vignettes=T)
+
+The 'build_vignettes' option makes it slower to install, but it provides the ability to say:
+
+browseVignettes("unpakathon")
+
+at the R prompt and choose among several tutorials on this data project

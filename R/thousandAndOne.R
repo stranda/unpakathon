fea570545d579fe3e658e5761674fab4c0f8098e
allan strand <stranda@gmail.com>
2021-06-28 08:40:01 -0400

    added locations

---
+++ R/thousandAndOne.R
@@ -0,0 +1,5 @@
+#' @name thousandAndOne
+#' @title 1001 genome accession information
+#' @description Information downloaded from the 1001 genomes about the natural accession lines.  Notably this includes genetic cluster from the 2016 Cell paper and also lat and long for each collection
+#' @format one row per natural accession
+NULL

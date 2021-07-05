0166043c712e5646d7686d518437d6291fd4a720
allan strand <stranda@gmail.com>
2021-07-05 15:07:46 -0400

    updated documentation

---
+++ R/high_impact_chr4.R
@@ -0,0 +1,9 @@
+#' @name high_impact_chr4
+#' @title polymorph1001 variant data for chr4 loci
+#' @doctype data
+#' @description Contains the variant information for all 1001 genome lines for each of the loci that were disrupted in the chr4 experiment
+#' @format one or more rows per locus/variant-type/strain(line) combination
+NULL
+
+
+

3d7d0f92463f558ec29ffbe3092031ea0d9aba02
allan strand <stranda@gmail.com>
2021-07-05 15:15:49 -0400

    renamed variants

--- R/variants_chr4.R
+++ R/variants_chr4.R
@@ -1,4 +1,4 @@
-#' @name high_impact_chr4
+#' @name variants_chr4
 #' @title polymorph1001 variant data for chr4 loci
 #' @doctype data
 #' @description Contains the variant information for all 1001 genome lines for each of the loci that were disrupted in the chr4 experiment
